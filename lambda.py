2e479fa28b8c721150f286673217b57c2d89ca81
Orçun Madran <orcunmadran@gmail.com>
2021-11-25 14:18:35 +0300

    Lambda örneği.

---
+++ lambda.py
@@ -0,0 +1,24 @@
+#Import the library
+from tkinter import *
+
+#Create an instance of Tkinter frame
+win= Tk()
+
+#Set the window geometry
+win.geometry("750x250")
+
+#Display a Label
+def print_text(text):
+   Label(win, text=text,font=('Helvetica 13 bold')).pack()
+
+btn1= Button(win, text="Button1" ,command= lambda:
+print_text("Button 1"))
+btn1.pack(pady=10)
+btn2= Button(win, text="Button2" ,command= lambda:
+print_text("Button 2"))
+btn2.pack(pady=10)
+btn3= Button(win, text="Button3" ,command= lambda:
+print_text("Button 3"))
+btn3.pack(pady=10)
+
+win.mainloop()
