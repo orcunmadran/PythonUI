c29e8a0837481897876e375ceeb290d9779f254e
orcunmadran <orcunmadran@gmail.com>
2021-12-02 17:02:51 +0300

    Albümün genel yapısı tamamlandı.

--- imageBank.py
+++ imageBank.py
@@ -3,8 +3,8 @@ from PIL import ImageTk, Image
 
 pencere = Tk()
 pencere.title("Resim Albümü")
-pencere.geometry("480x600")
-pencere.wm_iconbitmap("ico/kitap.ico")
+pencere.geometry("490x600")
+#pencere.wm_iconbitmap("ico/kitap.ico")
 
 baslik = Label(pencere, text="Sabahattin Ali'nin Kitapları")
 baslik.grid(row=0, column=0, padx= 10, pady=10)
@@ -12,15 +12,25 @@ baslik.config(font=("Arial", 30))
 
 kapaklar = ["img/kitap_01.jpg", "img/kitap_02.jpg", "img/kitap_03.jpg", "img/kitap_04.jpg", "img/kitap_05.jpg"]
 
-def goster(kapak):
+kapak = 0
+
+def goster():
+    global kapak
     gorsel = ImageTk.PhotoImage(Image.open(kapaklar[kapak]))
     cerceve = Label(image=gorsel)
     cerceve.image = gorsel
     cerceve.grid(row=1, column=0, padx= 10, pady=10)
+    if kapak < len(kapaklar)-1:
+        kapak +=1
+    else:
+        kapak = 0
 
-goster(0)
+goster()
 
-buton = Button(text="Yeni Kitap")
+def sonraki():
+    goster()
+
+buton = Button(text="Diğer Kitapları", command=sonraki)
 buton.grid(row=2, column=0, padx= 10, pady=10)
 buton.config(font=("Arial", 20))
 
