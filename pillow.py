20e0a2554b717874d797f25cd150fcdc4599c967
Orçun Madran <orcunmadran@gmail.com>
2021-12-02 14:29:50 +0300

    fotoğraf ve icon güncelleme sonrası örneklerin güncellenmesi

--- pillow.py
+++ pillow.py
@@ -3,17 +3,17 @@ from PIL import ImageTk, Image
 
 pencere = Tk()
 pencere.title("Otel Programı")
-pencere.iconbitmap("ico/otel.ico")
+pencere.iconbitmap("ico/kitap.ico")
 pencere.geometry("750x650")
 
-gorsel = ImageTk.PhotoImage(Image.open("img/otel.png"))
+gorsel = ImageTk.PhotoImage(Image.open("img/kitap_01.jpg"))
 
 def fotoTutucu():
     gorselTutucu = Label(image=gorsel)
     gorselTutucu.pack()
 
 
-butonum=Button(pencere, text="Foto Göster", command=fotoTutucu())
+butonum=Button(pencere, text="Foto Göster", command=fotoTutucu)
 butonum.pack()
 
 pencere.mainloop()
