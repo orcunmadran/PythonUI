45f8abf882b87e6c224438151558de67233fa046
orcunmadran <orcunmadran@gmail.com>
2021-12-02 11:31:53 +0300

    ico dosyası eklendi.

--- imageBank.py
+++ imageBank.py
@@ -7,7 +7,7 @@ pencere.iconbitmap("ico/otel.ico")
 pencere.geometry("750x650")
 
 goster = 0
-gorseller = ["img/a.png", "img/b.png"]
+gorseller = ["img/kitap_01.jpg", "img/kitap_02.jpg", "img/kitap_03.jpg", "img/kitap_04.jpg", "img/kitap_05.jpg"]
 gorsel = ImageTk.PhotoImage(Image.open(gorseller[goster]))
 gorselTutucu = Label(image=gorsel)
 gorselTutucu.pack()
